c1079b891dfaf257af73ae285f568e371af0bdbc
yash upadhyay <yashup1997@gmail.com>
2018-08-28 23:33:35 +0530

    app.py modified

--- first-application/app.py
+++ first-application/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,jsonify
 
 app = Flask(__name__)
 
@@ -23,7 +23,7 @@ stores=[
 
 #POST /store data:{name:}
 @app.route('/store',methods=['POST'])
-def create_store:
+def create_store():
     pass
 #GET /store/{string:name}
 @app.route('/store/<string:name>')
@@ -34,11 +34,11 @@ def get_store(name):
 
 #GET /store/
 @app.route('/store/')
-def get_stores(name):
-    pass
+def get_stores():
+    return jsonify({'stores':stores})
 #POST /store/<string:name>/item {name:,price}
 @app.route('/store/<string:name>/item',methods=['POST'])
-def create_item_in_store:
+def create_item_in_store():
     pass
 #GET /store/<string:name>/item
 @app.route('/store/<string:name>/item/')
