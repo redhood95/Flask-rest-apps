0b147b220904a80bcb6e45db9f4b8b0d72bd9890
yash upadhyay <yashup1997@gmail.com>
2018-08-28 23:53:47 +0530

    endpoints modified

--- first-application/app.py
+++ first-application/app.py
@@ -34,7 +34,6 @@ def create_store():
 #GET /store/{string:name}
 @app.route('/store/<string:name>')
 def get_store(name):
-    data = request.get_json()
     for store in stores:
         if store['name'] ==name:
             return jsonify(store)
@@ -49,9 +48,21 @@ def get_stores():
 #POST /store/<string:name>/item {name:,price}
 @app.route('/store/<string:name>/item',methods=['POST'])
 def create_item_in_store():
-    pass
+    request_data = request.get_json()
+    for store in stores:
+        if store['name'] == name;
+            new_item = {
+            'name' : request_data['name'],
+            'price':request_data['price']
+            }
+            store['item'].append(new_item)
+            return jsonify(new_item)
+
 #GET /store/<string:name>/item
 @app.route('/store/<string:name>/item/')
 def get_item_in_store(name):
-    pass
+    for store in stores:
+        if store['name'] ==name:
+            return jsonify({'items':store['items']})
+    return jsonify({'message':'store not found'})
 app.run(port = 5000)
