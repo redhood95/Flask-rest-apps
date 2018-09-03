4adf0ce48c36fd4191a76bae2fd77d0b5d59dd4d
yash upadhyay <yashup1997@gmail.com>
2018-09-03 23:28:32 +0530

    error msg added

--- first-restfulApp/app.py
+++ first-restfulApp/app.py
@@ -12,11 +12,12 @@ class Item(Resource):
         for item in items:
             if item['name'] == name:
                 return item
+        return {'item':None}, 404
 
     def post(self, name):
         item = {'name':name, 'price':12.00}
         items.append(item)
-        return item
+        return item, 201
 
 
 api.add_resource(Item, '/item/<string:name>')
