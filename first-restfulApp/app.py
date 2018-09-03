5fc9096fb74077838e5e9f421d144390bdc2637c
yash upadhyay <yashup1997@gmail.com>
2018-09-03 23:19:20 +0530

    edited class

--- first-restfulApp/app.py
+++ first-restfulApp/app.py
@@ -5,11 +5,17 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-class Student(Resource):
+items = []
+
+class Item(Resource):
     def get(self, name):
-        return {'Student' : name}
+        return {'item' : name}
+
+    def post(self, name):
+        item = {'name':name, 'price':12.00}
+        items.append(item)
 
 
-api.add_resource(Student, '/student/<string:name>')
+api.add_resource(Student, '/item/<string:name>')
 
 app.run(port=5000)
