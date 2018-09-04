520a628d1d2382eeb2d559a34f8ec1e09d8bdae0
yash upadhyay <yashup1997@gmail.com>
2018-09-04 17:00:39 +0530

    added auth and identity functions

--- first-restfulApp/security.py
+++ first-restfulApp/security.py
@@ -19,3 +19,12 @@ userid_mapping = {1:{
 'username':'bob',
 'password':'asdf'
 }}
+
+
+def authenticate(username, password):
+    user = username_mapping.get(username, None)
+    if user and user.password == password:
+        return user
+def identity(payload):
+    user_id = payload['identity']
+    retun user_id_mapping.get(user_id,None)
